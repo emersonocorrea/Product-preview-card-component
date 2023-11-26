3dec077fb0eeca84f497837fcf5a981ae106f0cf
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-11-26 19:41:52 -0300

    update

---
+++ src/scripts/tailwind.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        'fraunces': ['Fraunces', 'ui-sans-serif'],
+        'montserrat': ['Montserrat', 'ui-sans-serif']
+      },       
+      extend: {
+        colors: {
+          cream: '#F2EAE2',
+          pureWhite: '#fff',
+          aurometalSaurus: '#6C7289',
+          deepAquamarine: '#3D8168',
+          deepAquamarineHover: '#1A4032',
+          gunmetal: '#1C232B',
+        }
+      }
+    }
+  }
